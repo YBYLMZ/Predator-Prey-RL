5fd0201db9e45a1f5501cb51e294361d8de95dab
YBYLMZ <ybahadiryilmaz@gmail.com>
2022-05-13 20:36:45 +0300

    Add files via upload

--- dqn.py
+++ dqn.py
@@ -7,6 +7,7 @@ from stable_baselines3 import DQN, PPO
 from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.callbacks import CheckpointCallback
 from stable_baselines3.common.env_checker import check_env
+from sympy import true
 
 from predator_prey import QuadrotorFormation
 from marl_predator_prey import QuadrotorFormationMARL
@@ -16,11 +17,12 @@ QuadrotorFormation = QuadrotorFormationMARL
 
 # os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
-TOTAL_TIMESTEPS = 50_000_000
+TOTAL_TIMESTEPS = 1_000_000
 CHECKPOINT_FREQ = 100_000
 N_ENV = 8
-DEMO = False
-TEST = False
+DEMO = True
+TEST = True
+
 
 def make_env():
     def _init():
@@ -33,7 +35,8 @@ def make_env():
 if __name__ == '__main__':
     # callbacks
     if not TEST:
-        checkpoint_callback = CheckpointCallback(save_freq=CHECKPOINT_FREQ, save_path='./model_checkpoints/', name_prefix='rl_model_dyno_')
+        checkpoint_callback = CheckpointCallback(
+            save_freq=CHECKPOINT_FREQ, save_path='./model_checkpoints/', name_prefix='rl_model_dyno_')
     else:
         checkpoint_callback = None
 
@@ -41,16 +44,16 @@ if __name__ == '__main__':
 
     model = DQN("MlpPolicy", env,
                 tensorboard_log='./tensorboard_logs_dyno/', batch_size=32,
-                exploration_fraction=0.99,
-                verbose=2, policy_kwargs={"net_arch": [512, 512, 512]})
-    
+
+                verbose=2, policy_kwargs={"net_arch": [512, 512, 512]})  # exploration_fraction=0.99,
+
     if not TEST:
         model.learn(total_timesteps=TOTAL_TIMESTEPS, log_interval=5,
-                tb_log_name="dqn_log_dyno", callback=checkpoint_callback)
+                    tb_log_name="dqn_log_dyno", callback=checkpoint_callback)
         model.save("dqn_predator_dyno")
         del model
 
-    model.load("dqn_predator_dyno")
+    model = model.load("rl_model_dyno__700000_steps.zip")
     obs = env.reset()
     total_rew = 0
     rews = []
@@ -64,9 +67,9 @@ if __name__ == '__main__':
 
     env = QuadrotorFormation(visualization=False, moving_target=True)
     env.reset()
-
     if DEMO:
         i = 0
+        rewrrd = 0
         while i < 100:
             plt.cla()
 
@@ -83,7 +86,8 @@ if __name__ == '__main__':
 
                 i += 1
                 time.sleep(0.5)
-
+            print(total_rew)
+            print(action)
             RESOLUTION = 5
             # obstacles
 
